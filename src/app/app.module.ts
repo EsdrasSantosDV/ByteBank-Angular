1cf85a768a990405a06ae3115e13d66746b477ba
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-08-11 16:39:02 -0300

    CRIAR TRANSFERENCIA E LISTAR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
 
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferenica.compose';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.compose';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+
+registerLocaleData(localePt,'pt');
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponent
+    NovaTransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pt' },{provide:DEFAULT_CURRENCY_CODE,useValue:'BRL'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
